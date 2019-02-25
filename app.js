cd134e8a3e046975058a8246b83d672b9a4199ac
Cap Cod3r <vellackallintothomas@gmail.com>
2019-02-25 15:26:50 +0530

    UP

---
+++ app.js
@@ -0,0 +1,37 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require("mongoose");
+var indexRouter = require('./routes/api');
+
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(
+    env.mongo,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected..."));
+
+app.use('/', indexRouter);
+
+// port setting
+app.set("port", env.port);
+app.listen(app.get("port"), () => {
+  console.log(`Server started on port ` + app.get("port"));
+});
+
+module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt-nodejs');
+async function login(req,res){
+    User.findOne({'local.email':req.body.email},function(err,data){
+        if(err) console.log(err);
+        if(data){
+            if(validPassword(req.body.password,data.local.password)){
+                res.json({
+                    status:true,
+                    user:data
+                });
+            }
+            else{
+                res.json({
+                    status:false,
+                    msg:'Incorrect password'
+                });
+            }
+        }
+        else{
+            res.json({
+                status:false,
+                msg:'Invalid email'
+            });
+        }
+        
+    });
+}
+
+function validPassword(password,hash){
+    return bcrypt.compareSync(password, hash);
+}
+
+module.exports = {login};
